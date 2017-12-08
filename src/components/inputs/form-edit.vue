0abb23136472458f3618b586248958afada72b03
coolworld2015 <wintermute2015@ukr.net>
2017-12-08 15:24:45 +0200

    [inputs] - added inputs

--- src/components/inputs/form-edit.vue
+++ src/components/inputs/form-edit.vue
@@ -7,46 +7,78 @@
 
   <div v-else>
 	<div style="margin: auto; width: 25%; font-size: 22px; font-weight: bold; margin-bottom: 10px;">
-		<div style="text-align: center; padding-right: 40px;">{{ name }}</div>
+		<div style="text-align: center; padding-right: 40px;">{{ invoiceID }}</div>
 	</div>
     <form class="payment-form payment-form--create d-flex justify-content-stretch" autocomplete="off">
       <fieldset class="sender-data form-section-wrapper">
         <div class="form-section" style="width: 100%;">
           <div class="form-group">
-            <label for="senderSurname">Name</label>
-            <input type="text" class="form-control" id="senderSurname" placeholder="Name" v-model="name" readonly>
+            <label for="senderSurname">InvoiceID</label>
+            <input type="text" class="form-control" id="senderSurname" placeholder="invoiceID" v-model="invoiceID" readonly>
             <div class="invalid-feedback">
               Будь ласка, коректно вкажіть прізвище відправника.
             </div>
           </div>
-
+		  
+          <div class="form-group">
+            <label for="senderName">Date</label>
+            <input type="text" class="form-control" id="senderName" placeholder="date" v-model="date" readonly>
+            <div class="invalid-feedback">
+              Будь ласка, коректно вкажіть ім'я відправника.
+            </div>
+          </div>
+		  
+          <div class="form-group">
+            <label for="senderName">Project</label>
+            <input type="text" class="form-control" id="senderName" placeholder="Project" v-model="project" readonly>
+            <div class="invalid-feedback">
+              Будь ласка, коректно вкажіть ім'я відправника.
+            </div>
+          </div>
+		  
           <div class="form-group">
             <label for="senderName">Department</label>
             <input type="text" class="form-control" id="senderName" placeholder="Department" v-model="department" readonly>
             <div class="invalid-feedback">
               Будь ласка, коректно вкажіть ім'я відправника.
             </div>
-          </div>
+          </div>		  
 		  
           <div class="form-group">
-            <label for="senderName">Address</label>
-            <input type="text" class="form-control" id="senderName" placeholder="Address" v-model="address">
+            <label for="senderName">Employee</label>
+            <input type="text" class="form-control" id="senderName" placeholder="Employee" v-model="employee" readonly>
             <div class="invalid-feedback">
               Будь ласка, коректно вкажіть ім'я відправника.
             </div>
-          </div>
-
+          </div>	
+		  
           <div class="form-group">
-            <label for="senderPatronymic">Phone</label>
-            <input type="text" class="form-control" id="senderPatronymic1" placeholder="Phone" v-model="phone">
+            <label for="senderName">Product</label>
+            <input type="text" class="form-control" id="senderName" placeholder="Product" v-model="product" readonly>
             <div class="invalid-feedback">
-              Будь ласка, коректно вкажіть по-батькові відправника.
+              Будь ласка, коректно вкажіть ім'я відправника.
+            </div>
+          </div>		  
+		  
+          <div class="form-group">
+            <label for="senderName">Price</label>
+            <input type="text" class="form-control" id="senderName" placeholder="Price" v-model="price" readonly>
+            <div class="invalid-feedback">
+              Будь ласка, коректно вкажіть ім'я відправника.
+            </div>
+          </div>		 
+		  
+          <div class="form-group">
+            <label for="senderName">Quantity</label>
+            <input type="text" class="form-control" id="senderName" placeholder="Quantity" v-model="quantity" readonly>
+            <div class="invalid-feedback">
+              Будь ласка, коректно вкажіть ім'я відправника.
             </div>
           </div>
 		  
           <div class="form-group">
             <label for="senderPatronymic">Description</label>
-            <input type="text" class="form-control" id="senderPatronymic1" placeholder="Description" v-model="description">
+            <input type="text" class="form-control" id="senderPatronymic1" placeholder="Description" v-model="description" readonly>
             <div class="invalid-feedback">
               Будь ласка, коректно вкажіть по-батькові відправника.
             </div>
@@ -61,7 +93,7 @@
           </div>
  
 		  <div class="d-flex justify-content-center" style="margin-top: 30px;">
-			<button class="btn btn-danger" v-on:click="updateItem" style="margin: 10px; width: 100px; font-size: 14px;">Submit</button>
+			<button class="btn btn-danger" v-on:click="goBack" style="margin: 10px; width: 100px; font-size: 14px;">Back</button>
 			<button class="btn btn-danger" v-on:click="deleteConfirm" style="margin: 10px; width: 100px; font-size: 14px;">Delete</button>
 		  </div>
  
@@ -80,9 +112,9 @@ export default {
 	name: 'inputs-edit',
 	data() {
 		return {
-			name: '',
-			address: '',
-			phone: '',
+			invoiceID: '',
+			employee: '',
+			date: '',
 			description: '',
 			sum: '',
 			loading: false
@@ -113,11 +145,18 @@ export default {
 			if (appConfig) {
 				if (appConfig.input) {
 					this.id = appConfig.input.id;
-					this.name = appConfig.input.name;
-					this.address = appConfig.input.address;
-					this.phone = appConfig.input.phone;
+					this.invoiceID = appConfig.input.invoiceID;
+					this.date = appConfig.input.date;
+					this.project = appConfig.input.project;
+					this.projectID = appConfig.input.projectID;
+					this.employee = appConfig.input.employee;
+					this.employeeID = appConfig.input.employeeID;
 					this.department = appConfig.input.department;
 					this.departmentID = appConfig.input.departmentID;
+					this.product = appConfig.input.product;
+					this.productID = appConfig.input.productID;
+					this.price = appConfig.input.price;
+					this.quantity = appConfig.input.quantity;
 					this.sum = ((+appConfig.input.total).toFixed(2)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ");
 					this.description = appConfig.input.description;
 				}
