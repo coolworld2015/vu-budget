f714044ae836499a7d5c751dfa42ebdadddd3a7a
coolworld2015 <wintermute2015@ukr.net>
2018-01-09 14:55:24 +0200

    [reports] - updated reports

--- src/components/reports/report-details.vue
+++ src/components/reports/report-details.vue
@@ -7,7 +7,21 @@
   </div>
 
   <div v-else-if="status === 'show'" class="search-results-content">
-    <div class="payment" v-for="item in items" v-on:click="showDetails(item)">
+	<div style="border-style: groove; float: left; width: 49%; margin:1px;">
+    <div class="payment" v-for="item in inputs" v-on:click="showDetails(item)">
+		  <div class="search-results-item search-results-choose" style="width: 5%;"><span class="circle"></span></div>
+
+		  <div class="search-results-item search-results-sender" style="width: 15%;">{{ item.invoiceID }}</div>
+		  <div class="search-results-item search-results-sender" style="width: 20%; right: 15px;">{{ item.project }}</div>
+		  <div class="search-results-item search-results-sender" style="width: 20%; right: 40px;">{{ item.date }}</div>
+		  <div class="search-results-item search-results-sender" style="width: 20%; right: 10px;">{{ item.description }}</div>
+		  <div class="search-results-item search-results-transfer" style="width: 20%;">{{((+item.total).toFixed(2)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ")}}</div>
+
+	</div> 
+	</div>    
+	
+	<div style="border-style: groove; float: left; width: 49%; margin:1px;">
+	<div class="payment" v-for="item in outputs" v-on:click="showDetails(item)">
       <div class="search-results-item search-results-choose" style="width: 5%;"><span class="circle"></span></div>
 
       <div class="search-results-item search-results-sender" style="width: 15%;">{{ item.invoiceID }}</div>
@@ -15,7 +29,8 @@
       <div class="search-results-item search-results-sender" style="width: 20%; right: 40px;">{{ item.date }}</div>
       <div class="search-results-item search-results-sender" style="width: 20%; right: 10px;">{{ item.description }}</div>
       <div class="search-results-item search-results-transfer" style="width: 20%;">{{((+item.total).toFixed(2)).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1 ")}}</div>
-
+	
+	</div>
     </div>
   </div>
 
@@ -32,10 +47,11 @@ import Vue from 'vue';
 import appConfig from '../../main';
 
 export default {
-	name: 'inputs-items',
+	name: 'report-details',
 	data() {
 	  return {
-		items: [],
+		inputs: [],
+		outputs: [],
 		filteredItems: [],
 		recordsCount: 20,
 		positionY: 0,
@@ -44,43 +60,45 @@ export default {
 	  }
 	},
 	created() {
-		this.fetchData();
+		this.getInputs();
+		this.getOutputs()
+		
 		this.notification = {
 			title: 'Something went wrong',
 			message: 'Server responded with status code error',
 			important: true
 		}
-		appConfig.$on('searchQuery', searchQuery => {
-			this.searchQuery = searchQuery;
-			var arr = [].concat(appConfig.inputs.items);
-			var items = arr.filter((el) => el.invoiceID.indexOf(searchQuery.toLowerCase()) != -1);
-			this.filteredItems = items;
-			this.items = items.slice(0, 20);
-			this.positionY = 0;
-			this.recordsCount = 20;
-			
-			appConfig.$emit('itemsCount', items.length);
-			let total = 0;
-			this.items.forEach((el) => total += +el.total);
-			appConfig.$emit('total', total);
-					
-			if (searchQuery == '') {
-				this.items = appConfig.inputs.items.slice(0, 20);
-				this.filteredItems = appConfig.inputs.items;
-			}
-		})
 	},
 	methods: {
-		fetchData() {
+		getInputs() {
 			this.$http.get(appConfig.URL + 'inputs/get', {headers: {'Authorization': appConfig.access_token}})
 				.then(result => {
 					appConfig.inputs.items = result.data.sort(this.sort);
-					this.items = result.data.sort(this.sort).slice(0, 20);
+					this.inputs = result.data.sort(this.sort).slice(0, 20);
 					this.filteredItems = result.data.sort(this.sort);
 					this.status = 'show';
 					appConfig.$emit('itemsCount', result.data.length);
 					let total = 0;
-					this.items.forEach((el) => total += +el.total);
+					this.inputs.forEach((el) => total += +el.total);
+					appConfig.$emit('total', total);
+					setTimeout(()=>{document.querySelector('.search-results-content').addEventListener('scroll', this.handleScroll)}, 100);
+				}).catch((error)=> {
+				console.log(error)
+					appConfig.notifications.items.push(this.notification);
+					this.status = 'show';
+					this.$router.push('login');
+				})
+		},		
+		getOutputs() {
+			this.$http.get(appConfig.URL + 'outputs/get', {headers: {'Authorization': appConfig.access_token}})
+				.then(result => {
+					appConfig.outputs.items = result.data.sort(this.sort);
+					this.outputs = result.data.sort(this.sort).slice(0, 20);
+					this.filteredItems = result.data.sort(this.sort);
+					this.status = 'show';
+					appConfig.$emit('itemsCount', result.data.length);
+					let total = 0;
+					this.outputs.forEach((el) => total += +el.total);
 					appConfig.$emit('total', total);
 					setTimeout(()=>{document.querySelector('.search-results-content').addEventListener('scroll', this.handleScroll)}, 100);
 				}).catch((error)=> {
